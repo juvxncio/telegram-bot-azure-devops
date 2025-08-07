c235070c6e7a01b20b6d6d142a847e8ae156536f
juvxncio <bieljuvencio@gmail.com>
2025-08-07 17:03:28 -0300

    listando url dos times

--- api_azure.py
+++ api_azure.py
@@ -3,27 +3,32 @@ import os
 import requests
 from requests.auth import HTTPBasicAuth
 
-# from lista_url_times import lista_url_times
-
 load_dotenv()
 
 ORGANIZATION = os.getenv('ORGANIZATION')
 PAT = os.getenv('PAT')
 URL_BASE = os.getenv('URL_BASE')
 URL_PROJETOS = f'{URL_BASE}projects?api-version=7.0'
-URLS_TIMES = '{URL_BASE}projects/{project}/teams?api-version=7.0'
-
-response = requests.get(URL_PROJETOS, auth=HTTPBasicAuth('', PAT))
+URLS_TIMES = []
 
 lista_projetos = []
 
-if response.status_code == 200:
-    projetos = response.json()['value']
-    for projeto in projetos:
-        if not ('SUSPENSO' in projeto['name'].upper()):
-            lista_projetos.append(projeto['name'])
-else:
-    print('Não foi possível se conectar ao Azure DevOps')
+def puxar_projetos():
+
+    response = requests.get(URL_PROJETOS, auth=HTTPBasicAuth('', PAT))
+
+    if response.status_code == 200:
+        projetos = response.json()['value']
+        for projeto in projetos:
+            if not ('SUSPENSO' in projeto['name'].upper()):
+                lista_projetos.append(projeto['name'])
+    else:
+        print('Não foi possível se conectar ao Azure DevOps')
+
+puxar_projetos()
 
 for projeto in lista_projetos:
-    print(projeto + '\n')
+    URLS_TIMES.append(f'{URL_BASE}projects/{projeto}/teams?api-version=7.0')
+
+for url_time in URLS_TIMES:
+    print(url_time)
