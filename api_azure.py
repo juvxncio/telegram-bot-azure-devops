0496733c44edc463695979c09d254b835433eb48
juvxncio <bieljuvencio@gmail.com>
2025-08-08 12:05:16 -0300

    armazena apenas times ativos

--- api_azure.py
+++ api_azure.py
@@ -8,11 +8,15 @@ load_dotenv()
 ORGANIZATION = os.getenv('ORGANIZATION')
 PAT = os.getenv('PAT')
 URL_BASE = os.getenv('URL_BASE')
-URL_PROJETOS = f'{URL_BASE}projects?api-version=7.0'
+URL_PROJETOS = f'{URL_BASE}_apis/projects?api-version=7.0'
 
 url_times = []
+url_sprints = []
 lista_projetos = []
-lista_times = []
+lista_todos_times = []
+lista_times_ignorados_str = os.getenv('lista_times_ignorados')
+lista_times_ignorados = lista_times_ignorados_str.split(',')
+lista_times_ativos = []
 
 def puxar_projetos():
 
@@ -31,7 +35,7 @@ puxar_projetos()
 def puxar_times():
 
     for projeto in lista_projetos:
-        url_times.append(f'{URL_BASE}projects/{projeto}/teams?api-version=7.0')
+        url_times.append(f'{URL_BASE}_apis/projects/{projeto}/teams?api-version=7.0')
 
     for url_time in url_times:
         response = requests.get(url_time, auth=HTTPBasicAuth('', PAT))
@@ -39,8 +43,17 @@ def puxar_times():
         if response.status_code == 200:
             times = response.json()['value']
             for time in times:
-                lista_times.append(time['name'])
+                lista_todos_times.append(time['name'])
         else:
             print('Não foi possível se conectar ao Azure DevOps', response.status_code)
 
 puxar_times()
+
+for time in lista_todos_times:
+    if not any(palavra in time for palavra in lista_times_ignorados):
+        lista_times_ativos.append(time)
+
+# for time in lista_todos_times:
+#     url_sprints.append(f'{URL_BASE}{project}/{time}/_apis/work/teamsettings/iterations?api-version=7.0')
+
+print(type(lista_times_ignorados))
