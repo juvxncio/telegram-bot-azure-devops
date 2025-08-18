78d0bbdce9403892270cf96cc8f32526d236eb03
juvxncio <bieljuvencio@gmail.com>
2025-08-18 12:20:34 -0300

    o bot aceita mes e ano como parametro

--- bot.py
+++ bot.py
@@ -2,8 +2,9 @@ import os
 import logging
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
 import api_azure
+from datetime import datetime
 
 load_dotenv()
 
@@ -16,14 +17,33 @@ logging.basicConfig(
 
 
 async def horas(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    relatorio = api_azure.gera_relatorio()
-    await update.message.reply_text(relatorio)
+    try:
+        mes = None
+        ano = None
+
+        if len(context.args) >= 1:
+            mes = int(context.args[0])
+        else:
+            mes = datetime.now().month
+
+        if len(context.args) >= 2:
+            ano = int(context.args[1])
+        else:
+            ano = datetime.now().year
+
+        relatorio = api_azure.main(mes=mes, ano=ano)
+        await update.message.reply_text(relatorio)
+
+    except Exception as e:
+        await update.message.reply_text(f'❌ Erro ao gerar relatório: {str(e)}')
+
+
+def main():
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
+    application.add_handler(CommandHandler('horas', horas))
+    print('Bot iniciado. Aguardando comandos...')
+    application.run_polling()
 
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-
-    app.add_handler(CommandHandler(['horas'], horas))
-
-    print('Bot iniciado. Aguardando comandos...')
-    app.run_polling()
+    main()
