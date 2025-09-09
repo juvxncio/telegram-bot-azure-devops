8ef9ec80230737d602f397e589a40e5adbd3e857
juvxncio <bieljuvencio@gmail.com>
2025-09-09 11:32:55 -0300

    refatorado poo

--- bot/main.py
+++ bot/main.py
@@ -11,19 +11,21 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO,
 )
+logger = logging.getLogger(__name__)
 
 
 def main():
     application = Application.builder().token(TELEGRAM_TOKEN).build()
     application.add_handler(CommandHandler('start', handlers.start))
+    application.add_handler(CommandHandler('help', handlers.ajuda))
+    application.add_handler(CommandHandler('id', handlers.id))
     application.add_handler(CommandHandler('horas', handlers.horas))
     application.add_handler(CommandHandler('descricao', handlers.descricao))
     application.add_handler(CommandHandler('completo', handlers.completo))
     application.add_handler(CommandHandler('done', handlers.done))
     application.add_handler(CommandHandler('transbordo', handlers.transbordo))
-    application.add_handler(CommandHandler('id', handlers.id))
-    application.add_handler(CommandHandler('help', handlers.help))
     application.add_handler(CallbackQueryHandler(handlers.button_handler))
+    logger.info('Bot iniciado. Aguardando comandos...')
     print('Bot iniciado. Aguardando comandos...')
     application.run_polling()
 
